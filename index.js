027bcdadac0f762d7e72058ead93b54c63319bfa
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-06 13:07:08 +0300

    DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+})
+// console.log (
+//     "This console.log() fires when index.js loads - before DOMCOntentLoaded is triggered"
+// )
+
+
+// function changeToCool() {
+//     let text = document.querySelector('#text')
+//     text.textContent = "This is really cool!"
+//     // console.log(text)
+// }
+// console.log(changeToCool())
+// // changeToCool("This is really cool!")
